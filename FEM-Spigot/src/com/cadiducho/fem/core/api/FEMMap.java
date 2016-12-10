fbfaab535e8e57f14a410d088a7602368442365c
Cadox8 <cadox8@gmail.com>
2016-12-11 00:14:29 +0100

    Core: Añadida configuración al mapa

--- FEM-Spigot/src/com/cadiducho/fem/core/api/FEMMap.java
+++ FEM-Spigot/src/com/cadiducho/fem/core/api/FEMMap.java
@@ -1,30 +1,39 @@
 package com.cadiducho.fem.core.api;
 
+import com.cadiducho.fem.core.FEMCore;
+import com.google.common.io.Files;
+import lombok.Getter;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.Random;
 import java.util.logging.Level;
 
-import com.cadiducho.fem.core.FEMCore;
-import com.google.common.io.Files;
-
-import lombok.Getter;
-
 public class FEMMap {
 
 	//TODO: Hacer código más eficiente
 
-	@Getter
-	private String fileLocation = plugin.getDataFolder() + "/mapas";
-	@Getter
-	private String name;
-	@Getter
-	private String gameType; //Por si añadimos más mapas que no son del juego
+	@Getter private String fileLocation;
+	@Getter private String name;
+	@Getter private String gameType; //Por si añadimos más mapas que no son del juego
+    @Getter private String config;
 
 	private static final FEMCore plugin = FEMCore.getInstance();
 
-	public FEMMap(String name){
+    public FEMMap(String name){
+        this.name = name;
+        fileLocation = plugin.getDataFolder() + "/mapas/";
+        config = plugin.getDataFolder() + "/mapConfig/" + name + ".yml";
+
+        cambiarMapa();
+    }
+
+    @Deprecated
+	public FEMMap(String name, String game){
 		this.name = name;
+		this.gameType = game;
+		fileLocation = plugin.getDataFolder() + "/mapas/" + game + "/";
+        config = plugin.getDataFolder() + "/mapConfig/" + game + "/" + name + ".yml";
 
 		cambiarMapa();
 	}
@@ -33,19 +42,19 @@ public class FEMMap {
 	public void cambiarMapa(){
 		if(new File("world").delete()){ //Nombre del mapa principal
 			try{
-				Files.copy(getRandomMap(name), new File("world"));
+				Files.copy(getRandomMap(name), new File("world")); //Mapa
+				Files.copy(new File(config), new File(plugin.getDataFolder(), "config.yml")); //Config
 			}catch(IOException e){
-				plugin.log(Level.WARNING, "Error al copiar el mapa");
+				plugin.log(Level.WARNING, "Error al copiar el mapa o configuración");
 			}
 		}else{
-			plugin.log(Level.WARNING, "Error al cambiar de mapa");
+			plugin.log(Level.WARNING, "Error al cambiar de mapa o configuración");
 		}
 	}
 
-	@SuppressWarnings("null")
 	private File getRandomMap(String currentMap){
 		File[] dir = new File(fileLocation).listFiles(File::isDirectory);
-		File[] mapas = null;
+		File[] mapas = {};
 
 		for(int x = 0; x < dir.length; x++){
 			if(!dir[x].getName().equalsIgnoreCase(currentMap)){
@@ -53,7 +62,6 @@ public class FEMMap {
 				//¿Cambiar?
 			}
 		}
-
 		return mapas[new Random().nextInt(dir.length)];
 	}
 }
