43f90ae45e9a697ac80e92af216210f012e37d6f
Cadiducho <cadiducho@gmail.com>
2017-01-31 14:09:50 +0100

    Oppss

--- FEM-Spigot/src/org/inventivetalent/reflection/resolver/minecraft/NMSClassResolver.java
+++ FEM-Spigot/src/org/inventivetalent/reflection/resolver/minecraft/NMSClassResolver.java
@@ -25,7 +25,6 @@
  *  authors and contributors and should not be interpreted as representing official policies,
  *  either expressed or implied, of anybody else.
  */
-
 package org.inventivetalent.reflection.resolver.minecraft;
 
 import org.inventivetalent.reflection.minecraft.Minecraft;
@@ -36,13 +35,13 @@ import org.inventivetalent.reflection.resolver.ClassResolver;
  */
 public class NMSClassResolver extends ClassResolver {
 
-	@Override
-	public Class resolve(String... names) throws ClassNotFoundException {
-		for (int i = 0; i < names.length; i++) {
-			if (!names[i].startsWith("net.minecraft.server")) {
-				names[i] = "net.minecraft.server." + Minecraft.getVersion() + names[i];
-			}
-		}
-		return super.resolve(names);
-	}
+    @Override
+    public Class resolve(String... names) throws ClassNotFoundException {
+        for (int i = 0; i < names.length; i++) {
+            if (!names[i].startsWith("net.minecraft.server")) {
+                names[i] = "net.minecraft.server." + Minecraft.getVersion() + names[i];
+            }
+        }
+        return super.resolve(names);
+    }
 }
