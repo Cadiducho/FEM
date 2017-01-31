f1cf72a2c5168f2baf8c93d7062bce1b6cd040d9
Cadox8 <cadox8@gmail.com>
2017-01-31 14:38:50 +0100

    Lobby: Particulas

--- FEM-Lobby/src/com/cadiducho/fem/lobby/utils/MathsUtils.java
+++ FEM-Lobby/src/com/cadiducho/fem/lobby/utils/MathsUtils.java
@@ -1,91 +1,96 @@
 package com.cadiducho.fem.lobby.utils;
 
-import com.cadiducho.fem.core.particles.ParticleEffect;
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.Sound;
+import org.bukkit.*;
 import org.bukkit.entity.Player;
 import org.bukkit.util.Vector;
+import org.inventivetalent.particle.ParticleEffect;
+
+import java.util.Collection;
 
 public class MathsUtils {
 
-    private Player[] players = Bukkit.getOnlinePlayers().toArray(new Player[Bukkit.getOnlinePlayers().size()]);
+    private Collection<? extends Player> players = Bukkit.getOnlinePlayers();
+
+    private World world;
+    private double x;
+    private double y;
+    private double z;
 
     public void drawParticles(final Player p, final ParticleType pt){
-        Location l = p.getLocation();
+        world = p.getWorld();
+        x = p.getLocation().getX();
+        y = p.getLocation().getY();
+        z = p.getLocation().getZ();
 
         switch (pt.getPid()){
             case NONE:
-                drawFollowParticles(l, pt.getPe(), pt.getColor());
+                drawFollowParticles(pt.getPe(), pt.getColor());
                 break;
             case HALO:
-                drawCircle(l, pt.getPe(), pt.getColor(), 2);
+                drawCircle(pt.getPe(), pt.getColor(), 2);
                 break;
             case SPIRAL:
-                drawSpiral(l, pt.getPe(), pt.getColor());
+                drawSpiral(pt.getPe(), pt.getColor());
                 break;
             case SPHERE:
-                drawSphere(l, pt.getPe(), pt.getColor());
+                drawSphere(pt.getPe(), pt.getColor());
                 break;
             case WINGS:
-                drawWings(p.getEyeLocation(), pt.getPe(), pt.getColor());
+                x = p.getEyeLocation().getX();
+                y = p.getEyeLocation().getY();
+                z = p.getEyeLocation().getZ();
+                drawWings(pt.getPe(), pt.getColor());
                 break;
             case POLYGON:
-                drawPolygon(pt.getPoints(), p.getEyeLocation(), pt.getPe(), pt.getColor());
+                x = p.getEyeLocation().getX();
+                y = p.getEyeLocation().getY();
+                z = p.getEyeLocation().getZ();
+                drawPolygon(pt.getPoints(), pt.getPe(), pt.getColor());
                 break;
             case POLYGON_FULL:
-                drawFullPolygon(pt.getPoints(), p.getEyeLocation(), pt.getPe(), pt.getColor());
+                x = p.getEyeLocation().getX();
+                y = p.getEyeLocation().getY();
+                z = p.getEyeLocation().getZ();
+                drawFullPolygon(pt.getPoints(), pt.getPe(), pt.getColor());
                 break;
             case SHIELD:
-                drawShield(l, pt.getPe(), pt.getColor());
+                drawShield(pt.getPe(), pt.getColor());
                 break;
             case TORNADO:
-                drawTornado(l, pt.getPe(), pt.getColor());
+                drawTornado(pt.getPe(), pt.getColor());
                 break;
         }
     }
 
-    private void drawFollowParticles(final Location l, final ParticleEffect pe, final ParticleEffect.OrdinaryColor color){
-        if (pe.hasProperty(ParticleEffect.ParticleProperty.COLORABLE)) {
-            pe.display(color, l, players);
-            return;
-        }
-        pe.display((long)(l.getX()), (long)(l.getY()), (long)(l.getZ()), 0, 1, l, players);
+    private void drawFollowParticles(final ParticleEffect pe, final Color color){
+        pe.sendColor(players, x, y, z, color);
     }
 
-    private void drawSpiral(final Location l, final ParticleEffect pe, final ParticleEffect.OrdinaryColor color){
+    private void drawSpiral(final ParticleEffect pe, final Color color){
         final int radius = 2;
 
         for (double y = 0; y <= 5; y+=0.05) {
             double x = radius * Math.cos(y);
             double z = radius * Math.sin(y);
 
-            if (pe.hasProperty(ParticleEffect.ParticleProperty.COLORABLE)) {
-                pe.display(color, l, players);
-            } else {
-                pe.display((long) (l.getX() + x), (long) (l.getY() + y), (long) (l.getZ() + z), 0, 1, l, players);
-            }
+            pe.sendColor(players, x, y, z, color);
         }
     }
 
-    private void drawCircle(final Location l, final ParticleEffect pe, final ParticleEffect.OrdinaryColor color, int radius) {
+    private void drawCircle(final ParticleEffect pe, final Color color, int radius) {
         final int amount = 8;
         final double increment = (2 * Math.PI) / amount;
 
         for (int i = 0; i < amount; i++) {
             double angle = i * increment;
-            double x = l.getX() + (radius * Math.cos(angle));
-            double z = l.getZ() + (radius * Math.sin(angle));
+            double x = this.x + (radius * Math.cos(angle));
+            double z = this.z + (radius * Math.sin(angle));
 
-            if (pe.hasProperty(ParticleEffect.ParticleProperty.COLORABLE)) {
-                pe.display(color, l, players);
-            } else {
-            pe.display((long) (l.getX() + x), (long) (l.getY()), (long) (l.getZ() + z), 0, 1, l, players);
-            }
+            pe.sendColor(players, x, y, z, color);
         }
     }
 
-    private void drawShield(final Location l, final ParticleEffect pe, final ParticleEffect.OrdinaryColor color){
+    private void drawShield(final ParticleEffect pe, final Color color){
         int particles = 120;
         int particlesPerIteration = 8;
         float size = 1.0F;
@@ -112,31 +117,23 @@ public class MathsUtils {
 
             Utils.rotateVector(v, xRotation, yRotation, zRotation);
 
-            if (pe.hasProperty(ParticleEffect.ParticleProperty.COLORABLE)) {
-                pe.display(color, l, players);
-            } else {
-                pe.display(0.0F, 0.0F, 0.0F, 1.0F, 1, l, players);
-            }
-            l.subtract(l);
+            pe.sendColor(players, x, y, z, color);
         }
     }
 
-    private void drawTornado(final Location l, final ParticleEffect pe, final ParticleEffect.OrdinaryColor color){
+    private void drawTornado(final ParticleEffect pe, final Color color){
         float lineNumber = 3.0F;
         float j = 0.0F;
         float radius = 0.3F;
         float heightEcart = 0.01F;
+        Location l = new Location(world, x, y, z);
 
         l.setY(l.getY() + 0.5D);
 
         for (int i = 0; i < lineNumber; i++) {
             l.add(Math.cos(j) * radius, j * heightEcart,Math.sin(j) * radius);
 
-            if (pe.hasProperty(ParticleEffect.ParticleProperty.COLORABLE)) {
-                pe.display(color, l, players);
-            } else {
-                pe.display(0.0F, 0.0F, 0.0F, 1.0F, 1, l, players);
-            }
+            pe.sendColor(players, x, y, z, color);
         }
         j += 0.2F;
         if (j > 50.0F) {
@@ -144,28 +141,25 @@ public class MathsUtils {
         }
     }
 
-    private void drawSphere(final Location l, final ParticleEffect pe, final ParticleEffect.OrdinaryColor color){
+    private void drawSphere(final ParticleEffect pe, final Color color){
         for(double i = 0; i <= Math.PI; i += Math.PI / 10){
             double radius = Math.sin(i);
             double y = Math.cos(i);
             for(double a = 0; a < Math.PI * 2; a += Math.PI / 10) {
                 double x = Math.cos(a) * radius;
                 double z = Math.sin(a) * radius;
+                Location l = new Location(world, x, y, z);
 
                 l.add(x, y, z);
 
-                if (pe.hasProperty(ParticleEffect.ParticleProperty.COLORABLE)) {
-                    pe.display(color, l, players);
-                } else {
-                    pe.display(0.0F, 0.0F, 0.0F, 1.0F, 1, l, players);
-                }
+                pe.sendColor(players, x, y, z, color);
 
                 l.subtract(x, y, z);
             }
         }
     }
 
-    private void drawPolygon(final int points, final Location l, final ParticleEffect pe, final ParticleEffect.OrdinaryColor color){
+    private void drawPolygon(final int points, final ParticleEffect pe, final Color color){
         int t = 0;
         for(int iteration = 0; iteration < points; iteration++){
             double angle = 360.0 / points * iteration;
@@ -175,13 +169,11 @@ public class MathsUtils {
             double x = Math.cos(angle);
             double z = Math.sin(angle);
 
+            Location l = new Location(world, this.x, y, this.z);
+
             l.add(x, 0, z);
 
-            if (pe.hasProperty(ParticleEffect.ParticleProperty.COLORABLE)) {
-                pe.display(color, l, players);
-            } else {
-                pe.display(0.0F, 0.0F, 0.0F, 1.0F, 1, l, players);
-            }
+            pe.sendColor(players, x, y, z, color);
 
             l.subtract(x, 0, z);
 
@@ -189,7 +181,7 @@ public class MathsUtils {
 
             if (t == 6){
                 for (int r = 0; r < 6; r++) {
-                    drawCircle(l, pe, color, r);
+                    drawCircle(pe, color, r);
                     Bukkit.getOnlinePlayers().forEach(p -> p.playSound(l, Sound.FUSE, 1F, 1F));
 
                     if (r == 6)
@@ -199,7 +191,7 @@ public class MathsUtils {
         }
     }
 
-    private void drawFullPolygon(final int points, final Location l, final ParticleEffect pe, final ParticleEffect.OrdinaryColor color){
+    private void drawFullPolygon(final int points, final ParticleEffect pe, final Color color){
         int t = 0;
         for(int iteration = 0; iteration < points; iteration++){
             double angle = 360.0 / points * iteration;
@@ -216,14 +208,12 @@ public class MathsUtils {
             double deltaZ = z2 - z;
             double distance = Math.sqrt((deltaX - x) * (deltaX - x) + (deltaZ - z) * (deltaZ - z));
 
+            Location l = new Location(world, this.x, y, this.z);
+
             for(double d = 0; d < distance - .1; d += .1){
                 l.add(x + deltaX * d, 0, z + deltaZ * d);
 
-                if (pe.hasProperty(ParticleEffect.ParticleProperty.COLORABLE)) {
-                    pe.display(color, l, players);
-                } else {
-                    pe.display(0.0F, 0.0F, 0.0F, 1.0F, 1, l, players);
-                }
+                pe.sendColor(players, x, y, z, color);
 
                 l.subtract(x + deltaX * d, 0, z + deltaZ * d);
             }
@@ -232,7 +222,7 @@ public class MathsUtils {
 
             if (t == 6){
                 for (int r = 0; r < 6; r++) {
-                    drawCircle(l, pe, color, r);
+                    drawCircle(pe, color, r);
                     Bukkit.getOnlinePlayers().forEach(p -> p.playSound(l, Sound.FUSE, 1F, 1F));
 
                     if (r == 6)
@@ -243,7 +233,8 @@ public class MathsUtils {
     }
 
     //Location = EyeLocation
-    public void drawWings(final Location l, final ParticleEffect pe, final ParticleEffect.OrdinaryColor color){
+    public void drawWings(final ParticleEffect pe, final Color color){
+        Location l = new Location(world, this.x, y, this.z);
         Location loc = l.subtract(0.0D, 0.3D, 0.0D);
         loc.setPitch(0.0F);
         loc.setYaw(l.getYaw());
@@ -258,11 +249,7 @@ public class MathsUtils {
             Utils.rotateAroundAxisX(v, (loc.getPitch() + 90.0F) * 0.017453292F);
             Utils.rotateAroundAxisY(v, -loc.getYaw() * 0.017453292F);
 
-            if (pe.hasProperty(ParticleEffect.ParticleProperty.COLORABLE)) {
-                pe.display(color, l, players);
-            } else {
-                pe.display(0.0F, 0.0F, 0.0F, 1.0F, 1, l, players);
-            }
+            pe.sendColor(players, x, y, z, color);
         }
     }
 }
--- FEM-Lobby/src/com/cadiducho/fem/lobby/utils/ParticleType.java
+++ FEM-Lobby/src/com/cadiducho/fem/lobby/utils/ParticleType.java
@@ -1,52 +1,53 @@
 package com.cadiducho.fem.lobby.utils;
 
-import com.cadiducho.fem.core.particles.ParticleEffect;
 import com.cadiducho.fem.core.util.ItemUtil;
 import lombok.Getter;
+import org.bukkit.Color;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
+import org.inventivetalent.particle.ParticleEffect;
 
 import java.util.ArrayList;
 
 public enum ParticleType {
 
     //NORMAL
-    P(0, ItemUtil.createItem(Material.WOOL, 1, (short)14, "Corazones", new ArrayList<>()),ParticleEffect.HEART, ParticleID.NONE),
+    P(0, ItemUtil.createItem(Material.WOOL, 1, (short)14, "Corazones", new ArrayList<>()), ParticleEffect.HEART, ParticleID.NONE),
     P1(1, ItemUtil.createItem(Material.FURNACE, "Tren"), ParticleEffect.SMOKE_NORMAL, ParticleID.NONE),
     //HALO
     P2(2, ItemUtil.createItem(Material.STATIONARY_LAVA, "Anillo de Fuego"), ParticleEffect.DRIP_LAVA, ParticleID.HALO),
     P3(3, ItemUtil.createItem(Material.STATIONARY_WATER, "Anillo de Agua"), ParticleEffect.DRIP_WATER, ParticleID.HALO),
     //SPIRAL
     P4(4, ItemUtil.createItem(Material.FIREWORK, "Centellas"), ParticleEffect.FIREWORKS_SPARK, ParticleID.SPIRAL),
-    P5(5, ItemUtil.createItem(Material.RECORD_11, "Música"), ParticleEffect.NOTE, ParticleID.SPIRAL, new ParticleEffect.OrdinaryColor(50, 70, 100)),
+    P5(5, ItemUtil.createItem(Material.RECORD_11, "Música"), ParticleEffect.NOTE, ParticleID.SPIRAL, Color.fromBGR(50, 70, 100)),
     //SPHERE
-    P6(6, ItemUtil.createItem(Material.PORTAL, "Area Venenosa"), ParticleEffect.REDSTONE, ParticleID.SPHERE, new ParticleEffect.OrdinaryColor(255, 0, 255)),
+    P6(6, ItemUtil.createItem(Material.PORTAL, "Area Venenosa"), ParticleEffect.REDSTONE, ParticleID.SPHERE, Color.fromBGR(255, 0, 255)),
     //POLYGON
     P7(7, ItemUtil.createItem(Material.ENCHANTMENT_TABLE, "Magia Arcana"), ParticleEffect.ENCHANTMENT_TABLE, ParticleID.POLYGON, 5),
     //WINGS
-    P8(8, ItemUtil.createItem(Material.FEATHER, "Alas"), ParticleEffect.REDSTONE, ParticleID.WINGS, new ParticleEffect.OrdinaryColor(6, 200, 100));
+    P8(8, ItemUtil.createItem(Material.FEATHER, "Alas"), ParticleEffect.REDSTONE, ParticleID.WINGS, Color.fromBGR(6, 200, 100));
 
 
     @Getter private int id;
     @Getter private ItemStack item;
     @Getter private ParticleEffect pe;
     @Getter private ParticleID pid;
-    @Getter private ParticleEffect.OrdinaryColor color;
+    @Getter private Color color;
     @Getter private int points;
 
     ParticleType(int id, ItemStack i, ParticleEffect pe, ParticleID pid){
-        this(id, i, pe, pid, new ParticleEffect.OrdinaryColor(0, 0, 0), 0);
+        this(id, i, pe, pid, Color.fromBGR(0, 0, 0), 0);
     }
 
-    ParticleType(int id, ItemStack i, ParticleEffect pe, ParticleID pid, ParticleEffect.OrdinaryColor color){
+    ParticleType(int id, ItemStack i, ParticleEffect pe, ParticleID pid, Color color){
         this(id, i, pe, pid, color, 0);
     }
 
     ParticleType(int id, ItemStack i, ParticleEffect pe, ParticleID pid, int points){
-        this(id, i, pe, pid, new ParticleEffect.OrdinaryColor(0, 0, 0), points);
+        this(id, i, pe, pid, Color.fromBGR(0, 0, 0), points);
     }
 
-    ParticleType(int id, ItemStack i, ParticleEffect pe, ParticleID pid, ParticleEffect.OrdinaryColor color, int points){
+    ParticleType(int id, ItemStack i, ParticleEffect pe, ParticleID pid, Color color, int points){
         this.id = id;
         this.item = i;
         this.pe = pe;
