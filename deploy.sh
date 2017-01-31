3ad782c057ccc54bd6a3a6fea96d8e4e85e63529
Cadiducho <cadiducho@gmail.com>
2017-01-31 14:56:29 +0100

    Travis: mejorar script de deploy

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,17 @@
 FTP_USER=dev
 FTP_PASSWORD=plug1ns_123
+BRANCH_A_DEPLOY=develop
 
-for file in *.jar; do
-    for i in 1 2 3; do
-        echo Subiendo $file al servidor $i [137.74.81.20$((i-1))]
-        curl --ftp-create-dirs -T $file -u $FTP_USER:$FTP_PASSWORD ftp://137.74.81.20$((i-1))/$file
+branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+
+# Solo deploy al server si el branch es el deseado
+if [ "$branch" == "$BRANCH_A_DEPLOY" ]; then
+    for file in *.jar; do
+        for i in 1 2 3; do
+            echo Subiendo $file al servidor $i [137.74.81.20$((i-1))]
+            curl --ftp-create-dirs -T $file -u $FTP_USER:$FTP_PASSWORD ftp://137.74.81.20$((i-1))/$file
+        done
     done
-done
-
-echo Todos los archivos han sido subidos
+    echo Todos los archivos han sido subidos
+fi
+echo Deploy terminado
