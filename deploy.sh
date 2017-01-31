4ab499662703bd65be4c016dd44cfdf2d13f0912
Cadiducho <cadiducho@gmail.com>
2017-01-31 21:40:22 +0100

    Travis: Mensaje para no subir al servidor NoDeploy

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,22 @@
 FTP_USER=dev
 FTP_PASSWORD=plug1ns_123
 BRANCH_A_DEPLOY=develop
+NO_DEPLOY_MESSAGE=NoDeploy
 
-# Solo deploy al server si el branch es el deseado
-if [ "$TRAVIS_BRANCH" == "$BRANCH_A_DEPLOY" ]; then
-    for file in *.jar; do
-        for i in 1 2 3; do
-            echo Subiendo $file al servidor $i [137.74.81.20$((i-1))]
-            curl --ftp-create-dirs -T $file -u $FTP_USER:$FTP_PASSWORD ftp://137.74.81.20$((i-1))/$file
-        done
-    done
-    echo Todos los archivos han sido subidos
+if [[ "$TRAVIS_COMMIT_MESSAGE" == *"$NO_DEPLOY_MESSAGE"* ]]; then
+  echo "Esta commit ha sido marcada como no lista para subir al servidor"
 else
-    echo "El branch $branch no está preparado para ser subido al servidor"
+    # Solo deploy al server si el branch es el deseado
+    if [ "$TRAVIS_BRANCH" == "$BRANCH_A_DEPLOY" ]; then
+        for file in *.jar; do
+            for i in 1 2 3; do
+                echo Subiendo $file al servidor $i [137.74.81.20$((i-1))]
+                curl --ftp-create-dirs -T $file -u $FTP_USER:$FTP_PASSWORD ftp://137.74.81.20$((i-1))/$file
+            done
+        done
+        echo "Todos los archivos han sido subidos"
+    else
+        echo "El branch $branch no está preparado para ser subido al servidor"
+    fi
+    echo "Deploy al servidor terminado"
 fi
-echo Deploy terminado
