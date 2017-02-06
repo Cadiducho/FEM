3128b5f6cc177c3ee0ea5acfb92d540de464a791
Cadox8 <cadox8@gmail.com>
2017-02-06 20:01:30 +0100

    Core: Obtener versión del cliente

--- FEM-Spigot/src/com/cadiducho/fem/core/util/ReflectionAPI.java
+++ FEM-Spigot/src/com/cadiducho/fem/core/util/ReflectionAPI.java
@@ -1,27 +1,60 @@
 package com.cadiducho.fem.core.util;
 
+import com.cadiducho.fem.core.FEMCore;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.HashMap;
+import java.util.logging.Level;
 
 public class ReflectionAPI {
+
     private static HashMap<Class<? extends Entity>, Method> handles = new HashMap<Class<? extends Entity>, Method>();
     private static Field player_connection = null;
     private static Method player_sendPacket = null;
 
-    public static String getVersion(){
+    public static String getVersion() {
         String[] array = Bukkit.getServer().getClass().getPackage().getName().replace(".", ",").split(",");
         if (array.length == 4)
             return array[3] + ".";
         return "";
     }
 
-    public static Class<?> getNmsClass(String name){
+    public static int getPlayerVersion(Player p) {
+        try {
+            Class craftPlayerClass = Class.forName("org.bukkit.craftbukkit." + getVersion() + ".entity.CraftPlayer");
+            Class entityPlayerClass = Class.forName("net.minecraft.server." + getVersion() + ".EntityPlayer");
+            Class playerConnectionClass = Class.forName("net.minecraft.server." + getVersion() + ".PlayerConnection");
+            Class networkManagerClass = Class.forName("net.minecraft.server." + getVersion() + ".NetworkManager");
+
+            Method method = craftPlayerClass.getDeclaredMethod("getHandle");
+            method.setAccessible(true);
+            Object entityPlayer = method.invoke(p);
+
+            Field player = entityPlayerClass.getDeclaredField("playerConnection");
+            player.setAccessible(true);
+            Object playerConnection = player.get(entityPlayer);
+
+            Field network = playerConnectionClass.getDeclaredField("networkManager");
+            network.setAccessible(true);
+            Object networkManager = network.get(playerConnection);
+
+            Method version = networkManagerClass.getDeclaredMethod("getVersion");
+            method.setAccessible(true);
+            return (int) version.invoke(networkManager);
+
+        } catch (ClassNotFoundException | IllegalAccessException | NoSuchFieldException | NoSuchMethodException | InvocationTargetException e) {
+            FEMCore.getInstance().getLogger().log(Level.INFO, "Error al sacar la version del jugador");
+        }
+        return -1;
+    }
+
+    public static Class<?> getNmsClass(String name) {
         String version = getVersion();
         String className = "net.minecraft.server." + version + name;
         Class<?> clazz = null;
@@ -33,7 +66,7 @@ public class ReflectionAPI {
         return clazz;
     }
 
-    public static Field getFirstFieldByType(Class<?> clazz, Class<?> type){
+    public static Field getFirstFieldByType(Class<?> clazz, Class<?> type) {
         for (Field field : clazz.getDeclaredFields()) {
             field.setAccessible(true);
             if (field.getType() == type) {
@@ -46,8 +79,7 @@ public class ReflectionAPI {
     public static Field getField(Class<?> cl, String field_name) {
         try {
             return cl.getDeclaredField(field_name);
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
@@ -56,8 +88,7 @@ public class ReflectionAPI {
     public static Object getHandle(Entity entity) {
         try {
             return getMethod(entity.getClass(), "getHandle").invoke(entity);
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
@@ -66,8 +97,7 @@ public class ReflectionAPI {
     public static Object getHandle(World world) {
         try {
             return getMethod(world.getClass(), "getHandle").invoke(world);
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
@@ -106,8 +136,7 @@ public class ReflectionAPI {
             Object connection = connectionField.get(nmsPlayer);
             Method sendPacket = getMethod(connection.getClass(), "sendPacket");
             sendPacket.invoke(connection, packet);
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
